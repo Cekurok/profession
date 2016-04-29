3f0d31c11e61d50cdbf562b680481065e3520e5c
rangarajan <rrangarajan.85@gmail.com>
2016-04-29 04:57:31 -0700

    singleton added

--- programs/designpatterns/inc/singleton.h
+++ programs/designpatterns/inc/singleton.h
@@ -18,3 +18,26 @@
  * \copyright
  *   No part of this should be published in any form without the author permission
  */
+
+#ifndef __SINGLETON_H__
+
+#define __SINGLETON_H__
+
+class Singleton
+{
+
+public:
+	virtual ~Singleton();
+	Singleton *m_Singleton;
+
+	static Singleton* getInstance();
+	void singletonOperation();
+
+private:
+	static Singleton * instance;
+
+	Singleton();
+
+};
+
+#endif /*__SINGLETON_H__*/
--- programs/designpatterns/src/singleton.cpp
+++ programs/designpatterns/src/singleton.cpp
@@ -16,49 +16,39 @@
  */
  
 #include <iostream>
+#include "../inc/singleton.h"
 
 using namespace std;
- /* 
-  * Make the class responsible for its own
-  * global pointer and "initialization on
-  * first use" (by using a private static
-  * pointer and a public static accessor
-  * method).  The client uses only the public
-  * accessor method.
-  */
-class globalClass {
-    int value;
-    static globalClass *instance;
-    globalClass (int v = 0) { value = v;}
-    public :
-    int getvalue() { return value;}
-    void setvalue(int v) { value = v;}
-    static globalClass* getinstance() {
-	if(!instance)
-	    instance = new globalClass;
-    return instance;
-    }
-};
 
- /*
-  * Allocating and initializing GlobalClass's
-  * static data member.  The pointer is being
-  * allocated - not the object inself.
-  */
-globalClass *globalClass::instance = 0;
+Singleton * Singleton::instance = NULL;
+Singleton::Singleton(){
 
-void foo(void) {
-    globalClass::getinstance()->setvalue(1);
-    cout << "foo global ptr is " << globalClass::getinstance()->getvalue() << endl;
 }
 
-void bar(void) {
-    globalClass::getinstance()->setvalue(2);
-    cout << "bar global ptr is " << globalClass::getinstance()->getvalue() << endl;
+Singleton::~Singleton(){
+	delete instance;
 }
-int main() {
-    cout << "main global ptr is " << globalClass::getinstance()->getvalue() << endl;
-    foo();
-    bar();
- return 0;
+
+Singleton* Singleton::getInstance(){
+	if (instance == NULL)
+	{
+		instance = new Singleton();
+	}
+	
+	return  instance;
+}
+
+
+void Singleton::singletonOperation(){
+	cout << "singletonOperation" << endl;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+	Singleton * sg = Singleton::getInstance();
+	sg->singletonOperation();
+	
+	return 0;
 }
