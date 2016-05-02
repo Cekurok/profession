26aa08abb11b0727d03f404821e87bab74a6a12c
Proud Indian <rrangarajan.85@gmail.com>
2016-05-02 21:52:07 +0530

    separate compilation and doxygen support

---
+++ programs/logger/inc/logger.h
@@ -0,0 +1,117 @@
+/**
+ * \file   logger.h
+ * \author Rangarajan R 
+ * \date   May, 2016
+ * \brief  
+ *   Predict the output.
+ *
+ * \details 
+ *   Detailed description of file.
+ *
+ * \note
+ *   The notes for this file.
+ *
+ * \copyright
+ *   No part of this should be published in any form without the author 
+ *   permission
+ */
+ 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef __LOGGER_H__
+
+#define __LOGGER_H__
+
+ /**
+ * \fn    logger_create
+ * \brief   to create the logger to log.
+ * \details 
+ *   Detailed description of func provides certain actions as an example.
+ *
+ * \param [in] repeat  Number of times to do nothing.
+ *
+ * \retval TRUE   Successfully did nothing.
+ * \retval FALSE  Oops, did something.
+ *
+ */
+int logger_create(void);
+
+ /**
+ * \fn    Example function
+ * \brief   Example actions available.
+ * \details 
+ *   Detailed description of func provides certain actions as an example.
+ *
+ * \param [in] repeat  Number of times to do nothing.
+ *
+ * \retval TRUE   Successfully did nothing.
+ * \retval FALSE  Oops, did something.
+ *
+ */
+int logger_delete(void);
+
+ /**
+ * \fn    Example function
+ * \brief   Example actions available.
+ * \details 
+ *   Detailed description of func provides certain actions as an example.
+ *
+ * \param [in] repeat  Number of times to do nothing.
+ *
+ * \retval TRUE   Successfully did nothing.
+ * \retval FALSE  Oops, did something.
+ *
+ */
+int logger_read(void);
+
+ /**
+ * \fn    Example function
+ * \brief   Example actions available.
+ * \details 
+ *   Detailed description of func provides certain actions as an example.
+ *
+ * \param [in] repeat  Number of times to do nothing.
+ *
+ * \retval TRUE   Successfully did nothing.
+ * \retval FALSE  Oops, did something.
+ *
+ */
+int logger_write(void);
+
+ /**
+ * \fn    Example function
+ * \brief   Example actions available.
+ * \details 
+ *   Detailed description of func provides certain actions as an example.
+ *
+ * \param [in] repeat  Number of times to do nothing.
+ *
+ * \retval TRUE   Successfully did nothing.
+ * \retval FALSE  Oops, did something.
+ *
+ */
+int logger_open(void);
+
+ /**
+ * \fn    Example function
+ * \brief   Example actions available.
+ * \details 
+ *   Detailed description of func provides certain actions as an example.
+ *
+ * \param [in] repeat  Number of times to do nothing.
+ *
+ * \retval TRUE   Successfully did nothing.
+ * \retval FALSE  Oops, did something.
+ *
+ */
+int logger_close(void);
+
+#endif /* __LOGGER_H__*/
+
+#ifdef __cplusplus
+}
+#endif
+
+
