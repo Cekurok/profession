cec65e5de007323d6765a41ca8319e4f23146ef1
Proud Indian <rrangarajan.85@gmail.com>
2016-05-03 07:25:20 +0530

    server program

---
+++ programs/network/inc/server.h
@@ -0,0 +1,45 @@
+/**
+ * \file   server.h
+ * \author Rangarajan R 
+ * \date   May, 2016
+ * \brief  
+ *   Predict the output.
+ *
+ * \details 
+ *   Detailed description of file.
+ *
+ * \note
+ *   The notes for this file.
+ *
+ * \copyright
+ *   No part of this should be published in any form without the author 
+ *   permission
+ */
+ 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* File foo.  */
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+ /**
+ * \fn      dummy 
+ * \brief   Example actions available.
+ * \details 
+ *   Detailed description of func provides certain actions as an example.
+ *
+ * \param [in] repeat  Number of times to do nothing.
+ *
+ * \retval TRUE   Successfully did nothing.
+ * \retval FALSE  Oops, did something.
+ *
+ */
+int dummy(void);   
+     
+#endif /* __SERVER_H__ */
+
+#ifdef __cplusplus
+}
+#endif
